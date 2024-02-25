64265d47ab2d1220049eedad70f69d90a7780252
Phupa Denphatcharangkul <earthphupa@gmail.com>
2024-02-26 00:12:23 +0700

    scroll new

--- mainwindow.py
+++ mainwindow.py
@@ -15,9 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
-    QMainWindow, QPushButton, QScrollArea, QSizePolicy,
-    QStackedWidget, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QHBoxLayout,
+    QLabel, QLayout, QMainWindow, QPushButton,
+    QScrollArea, QSizePolicy, QStackedWidget, QVBoxLayout,
+    QWidget)
 import test_rc
 
 class Ui_MainWindow(object):
@@ -59,53 +60,153 @@ class Ui_MainWindow(object):
         self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 389, 749))
         self.verticalLayout_5 = QVBoxLayout(self.scrollAreaWidgetContents)
         self.verticalLayout_5.setObjectName(u"verticalLayout_5")
-        self.verticalLayout_5.setContentsMargins(0, 0, 0, 0)
-        self.logoHeader = QFrame(self.scrollAreaWidgetContents)
-        self.logoHeader.setObjectName(u"logoHeader")
-        self.logoHeader.setStyleSheet(u"background-image: url(:/images/image/sw_logo_section.svg);\n"
-"background-color: rgb(153, 255, 243);")
-        self.logoHeader.setFrameShape(QFrame.StyledPanel)
-        self.logoHeader.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_5.setSizeConstraint(QLayout.SetMaximumSize)
+        self.verticalLayout_5.setContentsMargins(0, 5, 0, 5)
+        self.frame_9 = QFrame(self.scrollAreaWidgetContents)
+        self.frame_9.setObjectName(u"frame_9")
+        self.frame_9.setStyleSheet(u"background-color: rgb(255, 138, 138);\n"
+"background-image: url(:/images/image/sw_logo_section.svg);")
+        self.frame_9.setFrameShape(QFrame.StyledPanel)
+        self.frame_9.setFrameShadow(QFrame.Raised)
 
-        self.verticalLayout_5.addWidget(self.logoHeader)
+        self.verticalLayout_5.addWidget(self.frame_9)
 
         self.frame_2 = QFrame(self.scrollAreaWidgetContents)
         self.frame_2.setObjectName(u"frame_2")
         self.frame_2.setStyleSheet(u"background-color: rgb(255, 233, 146);")
         self.frame_2.setFrameShape(QFrame.StyledPanel)
         self.frame_2.setFrameShadow(QFrame.Raised)
-        self.horizontalLayout_2 = QHBoxLayout(self.frame_2)
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.frame_3 = QFrame(self.frame_2)
-        self.frame_3.setObjectName(u"frame_3")
-        self.frame_3.setStyleSheet(u"background-color: rgb(79, 186, 116);")
-        self.frame_3.setFrameShape(QFrame.StyledPanel)
-        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_3 = QHBoxLayout(self.frame_2)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.frame_5 = QFrame(self.frame_2)
+        self.frame_5.setObjectName(u"frame_5")
+        self.frame_5.setStyleSheet(u"background-color: rgb(79, 186, 116);")
+        self.frame_5.setFrameShape(QFrame.StyledPanel)
+        self.frame_5.setFrameShadow(QFrame.Raised)
 
-        self.horizontalLayout_2.addWidget(self.frame_3)
+        self.horizontalLayout_3.addWidget(self.frame_5)
 
-        self.frame_4 = QFrame(self.frame_2)
-        self.frame_4.setObjectName(u"frame_4")
-        self.frame_4.setFrameShape(QFrame.StyledPanel)
-        self.frame_4.setFrameShadow(QFrame.Raised)
-        self.verticalLayout_6 = QVBoxLayout(self.frame_4)
-        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
-        self.label = QLabel(self.frame_4)
-        self.label.setObjectName(u"label")
+        self.frame_6 = QFrame(self.frame_2)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setFrameShape(QFrame.StyledPanel)
+        self.frame_6.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_7 = QVBoxLayout(self.frame_6)
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.label_3 = QLabel(self.frame_6)
+        self.label_3.setObjectName(u"label_3")
 
-        self.verticalLayout_6.addWidget(self.label)
+        self.verticalLayout_7.addWidget(self.label_3)
 
-        self.label_2 = QLabel(self.frame_4)
-        self.label_2.setObjectName(u"label_2")
+        self.label_4 = QLabel(self.frame_6)
+        self.label_4.setObjectName(u"label_4")
 
-        self.verticalLayout_6.addWidget(self.label_2)
+        self.verticalLayout_7.addWidget(self.label_4)
 
 
-        self.horizontalLayout_2.addWidget(self.frame_4)
+        self.horizontalLayout_3.addWidget(self.frame_6)
 
+        self.horizontalLayout_3.setStretch(0, 1)
+        self.horizontalLayout_3.setStretch(1, 20)
 
         self.verticalLayout_5.addWidget(self.frame_2)
 
+        self.frame_10 = QFrame(self.scrollAreaWidgetContents)
+        self.frame_10.setObjectName(u"frame_10")
+        self.frame_10.setFrameShape(QFrame.StyledPanel)
+        self.frame_10.setFrameShadow(QFrame.Raised)
+
+        self.verticalLayout_5.addWidget(self.frame_10)
+
+        self.frame_4 = QFrame(self.scrollAreaWidgetContents)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setStyleSheet(u"background-color: rgb(255, 233, 146);")
+        self.frame_4.setFrameShape(QFrame.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_4 = QHBoxLayout(self.frame_4)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout_4.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.frame_7 = QFrame(self.frame_4)
+        self.frame_7.setObjectName(u"frame_7")
+        self.frame_7.setStyleSheet(u"background-color: rgb(79, 186, 116);")
+        self.frame_7.setFrameShape(QFrame.StyledPanel)
+        self.frame_7.setFrameShadow(QFrame.Raised)
+
+        self.horizontalLayout_4.addWidget(self.frame_7)
+
+        self.frame_8 = QFrame(self.frame_4)
+        self.frame_8.setObjectName(u"frame_8")
+        self.frame_8.setFrameShape(QFrame.StyledPanel)
+        self.frame_8.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_8 = QVBoxLayout(self.frame_8)
+        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
+        self.label_5 = QLabel(self.frame_8)
+        self.label_5.setObjectName(u"label_5")
+
+        self.verticalLayout_8.addWidget(self.label_5)
+
+        self.label_6 = QLabel(self.frame_8)
+        self.label_6.setObjectName(u"label_6")
+
+        self.verticalLayout_8.addWidget(self.label_6)
+
+
+        self.horizontalLayout_4.addWidget(self.frame_8)
+
+        self.horizontalLayout_4.setStretch(0, 1)
+        self.horizontalLayout_4.setStretch(1, 20)
+
+        self.verticalLayout_5.addWidget(self.frame_4)
+
+        self.frame_3 = QFrame(self.scrollAreaWidgetContents)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setFrameShape(QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_6 = QVBoxLayout(self.frame_3)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.gridLayout_2 = QGridLayout()
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.pushButton_3 = QPushButton(self.frame_3)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.gridLayout_2.addWidget(self.pushButton_3, 1, 0, 1, 1)
+
+        self.pushButton_5 = QPushButton(self.frame_3)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+
+        self.gridLayout_2.addWidget(self.pushButton_5, 0, 2, 1, 1)
+
+        self.pushButton = QPushButton(self.frame_3)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.gridLayout_2.addWidget(self.pushButton, 0, 0, 1, 1)
+
+        self.pushButton_4 = QPushButton(self.frame_3)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+
+        self.gridLayout_2.addWidget(self.pushButton_4, 1, 1, 1, 1)
+
+        self.pushButton_2 = QPushButton(self.frame_3)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.gridLayout_2.addWidget(self.pushButton_2, 0, 1, 1, 1)
+
+        self.pushButton_6 = QPushButton(self.frame_3)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+
+        self.gridLayout_2.addWidget(self.pushButton_6, 0, 3, 1, 1)
+
+
+        self.verticalLayout_6.addLayout(self.gridLayout_2)
+
+
+        self.verticalLayout_5.addWidget(self.frame_3)
+
+        self.verticalLayout_5.setStretch(0, 10)
+        self.verticalLayout_5.setStretch(1, 5)
+        self.verticalLayout_5.setStretch(2, 20)
+        self.verticalLayout_5.setStretch(3, 5)
+        self.verticalLayout_5.setStretch(4, 20)
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
 
         self.verticalLayout_3.addWidget(self.scrollArea)
@@ -211,8 +312,16 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_5.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
         self.graphButton.setText(QCoreApplication.translate("MainWindow", u"chart-line", None))
         self.historyButton.setText(QCoreApplication.translate("MainWindow", u"history", None))
         self.dashboardButton.setText(QCoreApplication.translate("MainWindow", u"house", None))
