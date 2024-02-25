f090000884db6babc82cd84be7915b28d6f97306
Kasitphoom Thowongs <kasitphoom47@gmail.com>
2024-02-25 23:58:05 +0700

    add db

---
+++ db/init.py
@@ -0,0 +1,10 @@
+import ZODB, ZODB.config
+import class_obj, BTrees.OOBTree, transaction
+
+path = "./config.xml"
+db = ZODB.config.databaseFromURL(path)
+connection = db.open()
+root = connection.root
+
+root.accounts = BTrees.OOBTree.BTree()
+root.transactions = BTrees.OOBTree.BTree()
--- mainwindow.py
+++ mainwindow.py
@@ -17,7 +17,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QMainWindow,
     QPushButton, QSizePolicy, QVBoxLayout, QWidget)
-import test
+import test_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
