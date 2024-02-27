f6ae592dcb4176b83a6470ef2dfdd26da1b692ee
Kasitphoom Thowongs <kasitphoom47@gmail.com>
2024-02-27 12:00:51 +0700

    add update limit and check font

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PySide6 import QtCore, QtWidgets
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QFrame
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QFrame, QTreeWidget, QTreeWidgetItem
 from PySide6.QtCore import QSize, QRect, Qt
 from PySide6.QtGui import QFont, QFontDatabase, QMouseEvent
 from mainwindow import Ui_MainWindow
@@ -14,7 +14,6 @@ from obj.account import Account
 import pickle
 
 import ZODB, ZODB.config
-import BTrees.OOBTree, transaction
 
 path = "./db/config.xml"
 db = ZODB.config.databaseFromURL(path)
@@ -39,7 +38,11 @@ class MainWindow(QMainWindow):
         # Add fonts in QFontDatabase before setting up the UI
         QFontDatabase.addApplicationFont('otfs/Font Awesome 6 Brands-Regular-400.otf')
         QFontDatabase.addApplicationFont('otfs/Font Awesome 6 Free-Regular-400.otf')
-        QFontDatabase.addApplicationFont('otfs/Font Awesome 6 Free-Solid-900.otf')
+        fontID = QFontDatabase.addApplicationFont('otfs/Font Awesome 6 Free-Solid-900.otf')
+        QFontDatabase.addApplicationFont('otfs/Sushi Sushi.ttf')
+        if fontID < 0:
+            print('font not loaded')
+
         
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
@@ -51,8 +54,12 @@ class MainWindow(QMainWindow):
         # set text as this format XXX-X-1234-X
         self.ui.accountNumberlabel.setText(self.manager.get_account_number_non_visible())
         
+        self.ui.label_10.setFont(QFont("Sushi Sushi", 12))
+        
         # set balance
         self.ui.d_balance_amount.setText(self.manager.get_balance() + " THB")
+        
+        self.update_limit_dashboard()
 
     def eventFilter(self, obj, event):
         if type(event) == QMouseEvent and obj == self.ui.frame_12 and event.button() == Qt.MouseButton.LeftButton:
@@ -71,7 +78,11 @@ class MainWindow(QMainWindow):
             self.ui.accountNumberlabel.setText(self.manager.get_account_number_non_visible())
     
     def update_limit_dashboard(self):
-        pass
+        self.ui.foodlimitlabel.setText(f"{self.manager.calculate_limit('food')} THB")
+        self.ui.transportationlimitlabel.setText(f"{self.manager.calculate_limit('transport')} THB")
+        self.ui.entertainmentlimitlabel.setText(f"{self.manager.calculate_limit('entertainment')} THB")
+        self.ui.healthcarelimitlabel.setText(f"{self.manager.calculate_limit('healthcare')} THB")
+        self.ui.label_10.setText(f"{self.manager.calculate_limit('others')} THB")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
